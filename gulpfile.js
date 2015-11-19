9e07234515f52af31a47278d2d2d864deee8f77f
IzumiSy <beetle-noise@gmx.com>
2015-11-19 23:48:37 +0900

    Add reqiures for sass, jade, and concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
-var gulp = require("gulp")
-var sass = require("gulp-sass")
+var gulp   = require("gulp")
+var sass   = require("gulp-sass")
+var jade   = require("gulp-jade")
+var concat = require("gulp-concat")
 
 gulp.task("sass", function() { });
 
