2525eb0aab104dfa21a102916f1e31de02d811ca
IzumiSy <beetle-noise@gmx.com>
2015-11-19 23:51:36 +0900

    Add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp   = require("gulp")
 var sass   = require("gulp-sass")
 var jade   = require("gulp-jade")
 var concat = require("gulp-concat")
+var prefix = require("gulp-autoprefixer")
 
 gulp.task("sass", function() { });
 
