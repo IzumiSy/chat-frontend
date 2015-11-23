3bb7e411eb1faae80317778a9f549d754804a837
IzumiSy <beetle-noise@gmx.com>
2015-11-23 17:04:31 +0900

    Add gulp tasks and add some plugins for that

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,67 @@
-var gulp   = require("gulp")
-var sass   = require("gulp-sass")
-var jade   = require("gulp-jade")
-var concat = require("gulp-concat")
-var prefix = require("gulp-autoprefixer")
+var gulp     = require("gulp")
+var sass     = require("gulp-sass")
+var jade     = require("gulp-jade")
+var concat   = require("gulp-concat")
+var prefix   = require("gulp-autoprefixer")
+var header   = require("gulp-header")
+var server   = require("gulp-webserver")
+var plumber  = require("gulp-plumber")
+var del      = require("del")
+var sequence = require("run-sequence")
 
-gulp.task("sass", function() { });
+var sassTargets = [
+  "./styles/*.scss",
+  "./styles/**/*.scss",
+  "./styles/**/_*.scss"
+];
+var jadeTargets = [
+  "./jade/*.jade",
+  "./jade/**/*.jade",
+  "./jade/**/_*.jade"
+];
+var jsTargets = [
+  "./js/*.js",
+  "./js/**/*.js"
+];
+
+gulp.task("default", function() {
+  gulp.src("app")
+    .pipe(plumber())
+    .pipe(server({
+      livereload: true,
+      open: true
+     }));
+  gulp.watch(sassTargets, ["sass"]);
+  gulp.watch(jadeTargets, ["jade"]);
+  gulp.watch(jsTargets, ["concat-js"]);
+});
+
+gulp.task("sass", function() {
+  gulp.src(sassTargets)
+  .pipe(plumber())
+  .pipe(sass())
+  .pipe(header('@charset "utf-8;\n'))
+  .pipe(gulp.dest("./app/"))
+});
+
+gulp.task("jade", function() {
+  gulp.src(jadeTargets)
+  .pipe(plumber())
+  .pipe(jade({
+    pretty: true
+  }))
+  .pipe(gulp.dest("./app/"))
+});
+
+gulp.task("concat-js", function() {
+  var output = "app/application.js"
+  gulp.src(jsTargets)
+    .pipe(plumber())
+    .pipe(concat(output))
+    .pipe(gulp.dest("./app/"))
+});
+
+gulp.task("clean", function() {
+  del(["./app/*.*"]);
+});
 
