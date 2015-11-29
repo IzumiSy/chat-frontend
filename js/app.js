5a38e27159fffe41adff0bf99fb1d0d2210c9382
IzumiSy <beetle-noise@gmx.com>
2015-11-29 21:52:34 +0900

    Use router

--- js/app.js
+++ js/app.js
@@ -1,14 +1,27 @@
 Vue.use(VueRouter);
 
-var router = new VueRouter({
-  history: true,
-  saveScrollPosition: true
-});
+var app = Vue.extend({});
+var router = new VueRouter();
 
-var app = new Vue({
-  el: ".main-view",
-  data: {
-    text: "Hello World"
+// TODO
+// templates should be replaced
+// with contents of jade files.
+var components = {
+  root: Vue.extend({
+    template: "<div>root</div>"
+  }),
+  enterance: Vue.extend({
+    template: "<div>enterance</div>"
+  })
+};
+
+router.map({
+  "/": {
+    component: components.root
+  },
+  "/enter": {
+    component: components.enterance
   }
 });
+router.start(app, ".main-view-wrapper");
 
