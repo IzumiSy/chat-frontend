5f6c14dec07e7ec943aa28d628a81e80c4451976
IzumiSy <beetle-noise@gmx.com>
2015-11-30 23:51:12 +0900

    Add partial components

--- js/app.js
+++ js/app.js
@@ -9,8 +9,20 @@ var components = {
   }),
   entrance: Vue.extend({
     template: require("./entrance.html")
-  })
+  }),
+
+  partials: {
+    header: Vue.extend({
+      template: require("./components/_header.html")
+    }),
+    sidebar: Vue.extend({
+      template: require("./components/_sidebar.html")
+    })
+  }
 };
+
+Vue.component("va-header", components.partials.header);
+Vue.component("va-sidebar", components.partials.sidebar);
 
 router.map({
   "/": {
