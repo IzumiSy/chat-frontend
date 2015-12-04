20e4878cf9a4c000db4abbc03ee97d551c396efc
IzumiSy <beetle-noise@gmx.com>
2015-12-04 23:53:00 +0900

    Add methods section

--- js/components.js
+++ js/components.js
@@ -2,17 +2,25 @@ module.exports = {
   root: Vue.extend({
     template: require("./main.html")
   }),
+
   entrance: Vue.extend({
-    template: require("./entrance.html")
+    template: require("./entrance.html"),
+    methods: {
+      enterRobby: function() {
+        // Implement login action
+      }
+    }
    }),
 
   _partials: {
     header: Vue.extend({
       template: require("./components/_header.html")
     }),
+
     sidebar: Vue.extend({
       template: require("./components/_sidebar.html")
     }),
+
     messages: Vue.extend({
       template: require("./components/_messages.html")
     })
