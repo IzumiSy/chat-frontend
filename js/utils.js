aab803522f8931dac3e6f635418411ba57063db9
IzumiSy <beetle-noise@gmx.com>
2015-12-13 16:10:50 +0900

    Implement checkLogin function

--- js/utils.js
+++ js/utils.js
@@ -1,12 +1,14 @@
-var storage = require("./storage.js")
+var storage = require("./storage.js");
 
 var functions = {
   checkLogin: function() {
-    token = storage.get("token")
-    if (!token) {
-      // jump to entrance
+    token = storage.get("token");
+    if (token) {
+      return true;
+    } else {
+      return false;
     }
   }
-}
+};
 
 module.exports = functions;
