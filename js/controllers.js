c27d22f3084180051d242deb90ebe02714cad314
IzumiSy <beetle-noise@gmx.com>
2015-12-15 00:24:51 +0900

    Reduced nests

--- js/controllers.js
+++ js/controllers.js
@@ -4,45 +4,49 @@ var storage = require("./storage.js");
 var utils = require("./utils.js");
 var shared = require("./shared.js");
 
-var controllers = {
-  root: {
-    created: function() {
-      if (!utils.checkLogin()) {
-        shared.jumpers.entrance();
-      }
+var rootControllers = {
+  created: function() {
+    if (!utils.checkLogin()) {
+      shared.jumpers.entrance();
     }
+  }
+};
+
+var errorControllers = {
+  reload: function() {
+    shared.jumpers.root();
+  }
+};
+
+var entranceControllers = {
+  enterRobby: function() {
+    if (!this.username) {
+      // Shows an error message
+      return;
+    }
+
+    api.createNewUser(this.username, function(data, isSucceed) {
+      if (isSucceed) {
+        storage.set("token", data.token);
+        shared.jumpers.root();
+      } else {
+        shared.jumpers.error();
+      }
+    });
   },
 
-  error: {
-    reload: function() {
+  created: function() {
+    var token = storage.get("token");
+    if (token) {
       shared.jumpers.root();
     }
-  },
+  }
+};
 
-  entrance: {
-    enterRobby: function() {
-      if (!this.username) {
-        // Shows an error message
-        return;
-      }
-
-      api.createNewUser(this.username, function(data, isSucceed) {
-        if (isSucceed) {
-          storage.set("token", data.token);
-          shared.jumpers.root();
-        } else {
-          shared.jumpers.error();
-        }
-      });
-    },
-
-    created: function() {
-      var token = storage.get("token");
-      if (token) {
-        shared.jumpers.root();
-      }
-    }
-  },
+var controllers = {
+  root: rootControllers,
+  error: errorControllers,
+  entrance: entranceControllers,
 
   partials: {
     messageInput: {
