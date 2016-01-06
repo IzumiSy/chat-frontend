255d6d58f928a8827dd5247c43138551bd727c1b
IzumiSy <beetle-noise@gmx.com>
2016-01-06 22:33:37 +0900

    Partially implement message sending

--- js/controllers/messageInput.js
+++ js/controllers/messageInput.js
@@ -1,6 +1,23 @@
+var api = require("../api.js");
+var shared = require("../shared.js");
+
 var messageInputController = {
   sendMessage: function() {
-    // TODO send message
+    var message = this.message;
+    var currentRoomId = shared.data.currentRoomId;
+
+    if (!currentRoomId) {
+      console.warn("Error: currentRoomId is undefined or invalid.");
+      return;
+    }
+
+    api.sendMessage(currentRoomId, function(data, isSuccess) {
+      if (!isSuccess) {
+        console.warn("Error at api.sendMessage(...)");
+        return;
+      }
+      this.$set("message", null);
+    });
   }
 };
 
