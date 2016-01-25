d009a6bd1bc9c8162210e075b8b60c38a5605994
IzumiSy <beetle-noise@gmx.com>
2016-01-24 22:23:25 -0800

    Partially implement sent-time

--- js/controllers/root.js
+++ js/controllers/root.js
@@ -4,6 +4,13 @@
   var api = require("../api.js");
   var utils = require("../utils.js");
   var shared = require("../shared.js");
+  var _ = require("underscore");
+
+  var formatCreatedAtTime = function(data) {
+    var date = new Date(data.created_at);
+    data.formatted_created_at_time = (date.getHours() + ":" + date.getMinutes());
+    return data;
+  };
 
   var fetchUsersAndMessages = function(_this, roomId) {
     api.getRoomUsers(roomId, function(data, isSuccess) {
@@ -16,6 +23,7 @@
     });
     api.getRoomMessages(roomId, function(data, isSuccess) {
       if (isSuccess && data) {
+        data.forEach(formatCreatedAtTime);
         _this.$broadcast("app:msgView:setMessages", data);
       } else {
         console.warn("Error at api.getRoomMessages");
@@ -31,7 +39,9 @@
     shared.data.rocketio.instance = api.connectRocketIO(roomId);
     shared.data.rocketio.listeners.newMessage =
       shared.data.rocketio.instance.on("newMessage", function(data) {
-        _this.$broadcast("app:msgView:addMessage", JSON.parse(data));
+        data = JSON.parse(data);
+        formatCreatedAtTime(data);
+        _this.$broadcast("app:msgView:addMessage", data);
         _this.$broadcast("app:msgView:scrollBottom");
       });
   };
