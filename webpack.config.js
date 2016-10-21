12b66b605c46d06ca08d71327e8db0aec18a5431
IzumiSy <beetle-noise@gmx.com>
2016-10-21 19:30:58 +0900

    Read jade with pug-loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,13 @@ module.exports = {
   entry: __dirname + '/js/app.js',
   output: {
     path: __dirname + '/dist',
-    filename: '[name].js'
+    filename: 'app.js'
   },
   module: {
     loaders: [
       { test: /\.css$/, loader: ['style', 'css'] },
-      { test: /\.scss$/, loader: ['style', 'css', 'sass'] }
+      { test: /\.scss$/, loader: ['style', 'css', 'sass'] },
+      { test: /\.jade$/, loader: ['pug'] }
     ]
   }
 };
