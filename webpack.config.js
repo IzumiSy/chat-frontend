5c8564a4d4822ac9e3916d3f6644d43a684dd66c
IzumiSy <beetle-noise@gmx.com>
2016-10-21 19:56:44 +0900

    Add lots of plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var webpack = require('webpack')
+
 module.exports = {
   entry: __dirname + '/js/app.js',
   output: {
@@ -10,5 +12,15 @@ module.exports = {
       { test: /\.scss$/, loader: 'sass-loader' },
       { test: /\.jade$/, loader: 'pug-loader' }
     ]
-  }
+  },
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ]
 };
