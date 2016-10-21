63be81d0b0b9c22b79e984a3dd26a263bb7317c9
IzumiSy <beetle-noise@gmx.com>
2016-10-21 19:26:19 +0900

    Add a loader ffor scss

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,12 @@ module.exports = {
   entry: __dirname + '/js/app.js',
   output: {
     path: __dirname + '/dist',
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
     loaders: [
-      { test: /\.css$/, loader: 'style!css' },
+      { test: /\.css$/, loader: ['style', 'css'] },
+      { test: /\.scss$/, loader: ['style', 'css', 'sass'] }
     ]
   }
 };
