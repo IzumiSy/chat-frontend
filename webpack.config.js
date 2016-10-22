da92d27b0fa3e43352b08ecc78322f213c9aa29d
IzumiSy <beetle-noise@gmx.com>
2016-10-22 15:50:40 +0900

    Now svg is a loading target

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
         test: /\.jade$/,
         loader: 'pug-loader'
       }, {
-        test: /\.png$/,
+        test: /\.(png|svg)(\?.*)?$/,
         loader: 'file?name=[path][name].[ext]'
       }
     ]
