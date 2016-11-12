d5166a2a93b58c5ca803f255433cbffc9b4ad0f8
IzumiSy <beetle-noise@gmx.com>
2016-11-12 16:59:52 +0900

    Remove query section

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -47,10 +47,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          presets: ['es2015', 'stage-0']
-        }
+        loader: 'babel'
       },
       {
         test: /\.css$/,
