276601dce8f4d3546df8ae341f246301f9cc1dad
IzumiSy <beetle-noise@gmx.com>
2016-11-12 17:29:11 +0900

    Rewrite shared.js in ES6

--- src/shared.js
+++ src/shared.js
@@ -1,31 +1,27 @@
-(function() {
-  'use strict';
+const SharedStructure = function() {
+  return {
+    jumpers: {},
 
-  var SharedStructure = function() {
-    return {
-      jumpers: {},
+    data: {
+      channelMessages: {},
+      currentRoomId: null,
 
-      data: {
-        channelMessages: {},
-        currentRoomId: null,
-
-        rocketio: {
-          instance: null,
-          listeners: {}
-        },
+      rocketio: {
+        instance: null,
+        listeners: {}
       },
+    },
 
-      FACE_ASSETS: [
-        "1449951867",
-        "1449951870",
-        "1449951874",
-        "1449951898",
-        "1449951900",
-        "1449951968",
-        "1449951973"
-      ]
-    };
+    FACE_ASSETS: [
+      "1449951867",
+      "1449951870",
+      "1449951874",
+      "1449951898",
+      "1449951900",
+      "1449951968",
+      "1449951973"
+    ]
   };
+};
 
-  module.exports = new SharedStructure();
-})();
+export default new SharedStructure();
