c48a20103b913f7be3272576c9859692752cc8f9
IzumiSy <beetle-noise@gmx.com>
2016-11-20 21:11:13 +0900

    Use babel in gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,21 +1,20 @@
-var gulp   = require("gulp");
-var s3     = require("gulp-awspublish")
-var del    = require("del");
-var dotenv = require("dotenv");
+import gulp from "gulp";
+import s3 from "gulp-awspublish";
+import del from "del";
+import dotenv from "dotenv";
 
-const dest = "./dist";
+const dest = "dist";
 
-gulp.task("clean", function() {
+gulp.task("clean", () => {
   return del([
-    dest + "*.*",
-    dest + "/**/*",
-    "!" + dest + "/index.html"
+    `${dest}/**/*`,
+    `!${dest}/index.html`
   ]).then(function() {
     return process.exit(0);
   });
 });
 
-gulp.task("deploy-s3", function() {
+gulp.task("deploy-s3", () => {
   dotenv.config();
 
   var publisher = s3.create({
@@ -27,7 +26,7 @@ gulp.task("deploy-s3", function() {
     secretAccessKey: process.env.AWS_SECRET
   });
 
-  return gulp.src('dist/**/*')
+  return gulp.src(`${dest}/**/*`)
     .pipe(publisher.publish({}, { noAcl: true }))
     .pipe(publisher.cache())
     .pipe(publisher.sync())
