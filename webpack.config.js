85a8a2e709ee9c5427e80e963ea927b500eb80e0
IzumiSy <beetle-noise@gmx.com>
2017-12-17 19:18:03 +0900

    Require babel-plugin-add-module-exports

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,8 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
+            presets: ['@babel/preset-env'],
+            plugins: [require('babel-plugin-add-module-exports')]
           }
         }
       }, {
